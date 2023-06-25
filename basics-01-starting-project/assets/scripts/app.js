83628ab1765713005fa467d1ac49314747728db5
kmert0@outlook.com <kmert0@outlook.com>
2023-06-25 21:52:01 +0300

    Adding a Custom Function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert(`The result is: ${result} `);
+}
+
+add(1, 2);
+add(5, 9);
+
 currentResult = currentResult + 10;
 
 let calculationDescription = `(  ${currentResult} + 10) * 2`;
