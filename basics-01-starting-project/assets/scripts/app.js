4a5df09f01189b88670420d06029778e603f1525
kmert0@outlook.com <kmert0@outlook.com>
2023-06-25 11:33:11 +0300

    More on Strings

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,6 +3,7 @@ let currentResult = defaultResult;
 
 currentResult = currentResult + 10;
 
-let calculationDescription = '(' + currentResult + ' + 10) * 2'
+let calculationDescription = `(  ${currentResult} + 10) * 2`;
+let errorMessage = "An error occurred!";
 
 outputResult(currentResult, calculationDescription);
