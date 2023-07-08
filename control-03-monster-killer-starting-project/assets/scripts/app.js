3103d4732758ca4d544f4424962729be3443a093
Ramazan Mert Kaya <kmert0@outlook.com>
2023-07-08 23:23:17 +0300

    for-of Loop

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -215,14 +215,22 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  // for (let i = 0; i < 3; i++) {
-  //   console.log('------------');
-  // }
+  for (let i = 0; i < 3; i++) {
+    console.log('------------');
+  }
   // for (let i = 10; i > 0;) {
   //   i--;
   //   console.log(i);
   // }
-  console.log(battleLog);
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log(battleLog[i]);
+  // }
+  let i = 0;
+  for (const logEntry of battleLog) {
+    console.log(logEntry);
+    console.log(i);
+    i++;
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
