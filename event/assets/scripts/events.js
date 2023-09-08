50896cb731643ad1d53d0375d515f3d7c3420dc4
Ramazan Mert Kaya <kmert0@outlook.com>
2023-09-08 13:26:25 +0300

    Capturing & Bubbling

--- event/assets/scripts/events.js
+++ event/assets/scripts/events.js
@@ -1,13 +1,14 @@
 const button = document.querySelector("button");
 const form = document.querySelector("form");
+const div = document.querySelector("div");
 
-const buttonClickHandler = (event) => {
-  event.target.disabled = true;
-  console.log(event);
-};
+// const buttonClickHandler = (event) => {
+//   event.target.disabled = true;
+//   console.log(event);
+// };
 
 // button.addEventListener("click", buttonClickHandler);
-button.addEventListener("mouseenter", buttonClickHandler);
+// button.addEventListener("mouseenter", buttonClickHandler);
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formInputs = event.target.querySelectorAll("input");
@@ -19,3 +20,14 @@ form.addEventListener("submit", (event) => {
 // setTimeout(() => {
 //   button.removeEventListener("click", buttonClickHandler);
 // }, 2000);
+
+div.addEventListener("click", (event) => {
+  console.log("CLICKED DIV");
+  console.log(event);
+});
+
+button.addEventListener("click", (event) => {
+  event.stopPropagation();
+  console.log("CLICKED BUTTON");
+  console.log(event);
+});
