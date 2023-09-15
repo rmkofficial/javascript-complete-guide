d7d5581caca38ac4f591abef515859aaf8db1a0e
Ramazan Mert Kaya <kmert0@outlook.com>
2023-09-15 13:58:26 +0300

    Recursion

--- advanced function/app.js
+++ advanced function/app.js
@@ -1,26 +1,43 @@
-function add(a, b) {
-  return a + b;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-console.log(add(1, 2));
-console.log(add(5, 10));
+// function sendDataToServer() {}
 
-//
+console.log(add(1, 5)); // 6
+console.log(add(12, 15)); // 27
 
-const hobbies = ["Sports", "Cooking"];
+function addRandom(num1) {
+  return num1 + Math.random();
+}
+
+console.log(addRandom(5));
+
+let previousResult = 0;
+
+function addMoreNumbers(num1, num2) {
+  const sum = num1 + num2;
+  previousResult = sum;
+  return sum;
+}
+
+console.log(addMoreNumbers(1, 5));
+
+const hobbies = ['Sports', 'Cooking'];
 
 function printHobbies(h) {
-  h.push("NEW HOBBY");
+  h.push('NEW HOBBY');
   console.log(h);
 }
 
 printHobbies(hobbies);
 
-//
+let multiplier = 1.1;
 
 function createTaxCalculator(tax) {
   function calculateTax(amount) {
-    return amount * tax;
+    console.log(multiplier);
+    return amount * tax * multiplier;
   }
 
   return calculateTax;
@@ -29,18 +46,86 @@ function createTaxCalculator(tax) {
 const calculateVatAmount = createTaxCalculator(0.19);
 const calculateIncomeTaxAmount = createTaxCalculator(0.25);
 
+// multiplier = 1.2;
+
 console.log(calculateVatAmount(100));
-console.log(calculateIncomeTaxAmount(200));
+console.log(calculateVatAmount(200));
 
-//
-
-let userName = "Max";
+let userName = 'Max';
 
 function greetUser() {
-  // let name = userName;
-  console.log("Hi " + name);
+  // let name = 'Anna';
+  console.log('Hi ' + name);
 }
 
-let name = "Anna";
+let name = 'Maximilian';
+
+userName = 'Manuel';
 
 greetUser();
+
+
+// function powerOf(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+
+function powerOf(x, n) {
+
+  // if (n === 1) {
+  //   return x;
+  // }
+  // return x * powerOf(x, n - 1);
+
+  return n === 1 ? x : x * powerOf(x, n - 1);
+}
+
+console.log(powerOf(2, 3)); // 2 * 2 * 2
+
+const myself = {
+  name: 'Max',
+  friends: [
+    {
+      name: 'Manuel',
+      friends: [
+        {
+          name: 'Chris',
+          friends: [
+            {
+              name: 'Hari'
+            },
+            {
+              name: 'Amilia'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Julia'
+    }
+  ]
+};
+
+function getFriendNames(person) {
+  const collectedNames = [];
+
+  if (!person.friends) {
+    return [];
+  }
+  
+  for (const friend of person.friends) {
+    collectedNames.push(friend.name);
+    collectedNames.push(...getFriendNames(friend));
+  }
+  
+  return collectedNames;
+}
+
+console.log(getFriendNames(myself));
