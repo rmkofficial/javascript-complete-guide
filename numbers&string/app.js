e88d86d3b97779ea2b6dc34b92f31b8f158803e3
Ramazan Mert Kaya <kmert0@outlook.com>
2023-09-20 16:32:44 +0300

    Tagged Templates

--- numbers&string/app.js
+++ numbers&string/app.js
@@ -3,3 +3,13 @@ function randomIntBetween(min, max) {
 }
 
 console.log(randomIntBetween(1, 10));
+
+function productDescription(strings, productName, productPrice) {
+  return { name: productName, price: productPrice };
+}
+
+const prodName = "JavaScript Course";
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+console.log(productOutput);
