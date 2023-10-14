f49fe33149f530fd22160cafbd2197fd7bfefcbf
Ramazan Mert Kaya <kmert0@outlook.com>
2023-10-14 11:00:08 +0300

    Triggering Requests via the UI

--- http/assets/scripts/app.js
+++ http/assets/scripts/app.js
@@ -1,5 +1,7 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -45,5 +47,11 @@ async function createPost(title, content) {
   sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
 }
 
-fetchPosts();
-createPost("DUMMY", "A dummy post!");
+fetchButton.addEventListener("click", fetchPosts);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+
+  createPost(enteredTitle, enteredContent);
+});
