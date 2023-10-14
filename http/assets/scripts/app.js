a5b6f57cbe62cb966c4825596b570e5d95c6bd23
Ramazan Mert Kaya <kmert0@outlook.com>
2023-10-14 10:43:17 +0300

    Promisifying Http Requests

--- http/assets/scripts/app.js
+++ http/assets/scripts/app.js
@@ -1,21 +1,36 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xhr.open(method, url);
 
-xhr.responseType = "json";
+    xhr.responseType = "json";
 
-xhr.onload = () => {
-  const listOfPosts = xhr.response;
-  console.log(listOfPosts);
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+
+    xhr.send();
+  });
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEL = document.importNode(postTemplate.content, true);
     postEL.querySelector("h2").textContent = post.title.toUpperCase();
     postEL.querySelector("p").textContent = post.body;
     listElement.append(postEL);
   }
-};
+}
 
-xhr.send();
+fetchPosts();
